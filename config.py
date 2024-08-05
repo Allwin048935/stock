54d94347400af72a8c46c7ce7ae46ba4b0896c1c
Allwin048935 <156902166+Allwin048935@users.noreply.github.com>
2024-08-05 09:50:58 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+
+TELEGRAM_TOKEN = 'your_telegram_bot_token_here'
+CHAT_ID = 'your_telegram_chat_id_here'
+SELECTED_SYMBOLS = ['RELIANCE.NS', 'TCS.NS', 'INFY.NS']  # Replace with your chosen NSE symbols
